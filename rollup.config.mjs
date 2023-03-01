4b8a34a6903e2467bb3066afaaf3844312db5b49
Andy Chubb <achubb@fastmail.fm>
2023-03-01 17:16:16 +0000

    chore: trying rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,37 +1,14 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import json from "@rollup/plugin-json";
+import typescript from '@rollup/plugin-typescript';
 
-// const packageJson = require("./package.json");
-import packageJson from "./package.json" assert { type: "json" };
-
-export default [
-	{
-		input: "src/main.ts",
-		output: [
-			{
-				file: packageJson.main,
-				format: "cjs",
-				sourcemap: true,
-			},
-			{
-				file: packageJson.module,
-				format: "esm",
-				sourcemap: true,
-			},
-		],
-		plugins: [
-            json(),
-			resolve(),
-			commonjs(),
-			typescript({ tsconfig: "./tsconfig.json" }),
-		],
-	},
-	{
-		input: "dist/esm/types/main.d.ts",
-		output: [{ file: "dist/main.d.ts", format: "esm" }],
-		plugins: [dts()]
-	},
-];
+export default {
+  input: 'src/main.ts',
+  output: {
+    file: 'dist/main.js',
+    format: 'cjs'
+  },
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ]
+};
