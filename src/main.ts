0eb78b49efb9a89495ec38cb2cd69a4e4f6847bb
Andy Chubb <achubb@fastmail.fm>
2023-03-03 11:40:34 +0000

    chore: changing initial run function

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,21 @@
-import * as core from "@actions/core"
-import * as github from "@actions/github"
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 import { lint, formatResult } from './lint';
 
-function getPrTitle(): string | undefined {
-    let prTitle: string | undefined
-    const pullRequest = github.context.payload.pull_request
-
-    if(pullRequest && pullRequest.title) {
-        prTitle = pullRequest.title as string
-    } else {
-        prTitle = undefined
-    }
-    return prTitle
-}
-
-async function run(): Promise<void> {
-    const title = getPrTitle()
+void (async function run() {
+    const title = github.context.payload.pull_request?.title as string;
     const configFile = core.getInput('commitlintConfigFile');
 
-    core.info(`Checking if the title of this PR "${title ?? ""}" meets the requirements ...`);
+    core.info(`🔎 Checking if the title of this PR "${title}" meets the requirements ...`);
 
-    if (title) {
-        try {
-            const lintResult = await lint(title, configFile);
-            if (!lintResult.valid) {
-                core.setFailed(`\n ${formatResult(lintResult)}`);
-            } else {
-                core.info('✔️ All good');
-            }
-        } catch (error) {
-            core.setFailed(error as Error);
+    try {
+        const lintResult = await lint(title, configFile);
+        if (!lintResult.valid) {
+            core.setFailed(`\n ${formatResult(lintResult)}`);
+        } else {
+            core.info('✔️ All good');
         }
+    } catch (error) {
+        core.setFailed(error as Error);
     }
-}
-
-void run()
+})();
