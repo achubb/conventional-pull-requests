30c92c905906fba937f79db7577437832c48742b
Andy Chubb <achubb@fastmail.fm>
2023-03-03 12:52:49 +0000

    chore: tidyup

--- commitlint.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = { rules: { "scope-case": [2, "always", "lower-case"] } };
--- types/lint-pr.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function lintPR(title: string, configurationPath: string): Promise<void>;
-//# sourceMappingURL=lint-pr.d.ts.map
